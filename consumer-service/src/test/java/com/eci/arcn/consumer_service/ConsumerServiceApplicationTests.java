71fed5aa8f96b4e528c780e55e853ff145b6698c
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2026-04-25 22:19:05 -0500

    Add producer-service, consumer-service and docker-compose

---
+++ consumer-service/src/test/java/com/eci/arcn/consumer_service/ConsumerServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eci.arcn.consumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ producer-service/src/test/java/com/eci/arcn/producer_service/ProducerServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eci.arcn.producer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
